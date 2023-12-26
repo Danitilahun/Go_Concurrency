4ecd9568a091485428daac86eee1752db4d6c7c1
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-27 00:23:38 +0300

    reproducing a problem of negative WaitGroup counter adding done without adding or calling done morethan adding

---
+++ wait_group/common_waitGroup_problems/less_done.go
@@ -0,0 +1,16 @@
+package main
+
+// import (
+// 	"sync"
+// )
+
+// // Calling Done more times than Add panics
+// // Calling Done less times than Add results in deadlock
+// func main() {
+// 	var wg sync.WaitGroup
+// 	wg.Add(1) // Adds one task to the WaitGroup
+
+// 	// No call to wg.Done() to signal task completion
+// 	// This will lead to both a panic and a deadlock
+// 	wg.Wait() // Wait indefinitely for the task to complete
+// }
