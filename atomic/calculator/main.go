d3515015b809fe391d0c468912def12864e0a32b
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-27 22:28:42 +0300

    Adding all operation

--- atomic/calculator/main.go
+++ atomic/calculator/main.go
@@ -13,3 +13,30 @@ func newCalculator() calculator {
 	c.res.Store(float64(0))
 	return c
 }
+
+func (c *calculator) result() float64 {
+	r, ok := c.res.Load().(float64)
+	if !ok {
+		panic("operating with wrong type")
+	}
+	return r
+}
+
+func (c *calculator) add(n float64) {
+	c.res.Store(c.result() + n)
+}
+
+func (c *calculator) sub(n float64) {
+	c.res.Store(c.result() - n)
+}
+
+func (c *calculator) mul(n float64) {
+	c.res.Store(c.result() * n)
+}
+
+func (c *calculator) div(n float64) {
+	if n == 0 {
+		panic("cannot divide by zero")
+	}
+	c.res.Store(c.result() / n)
+}
